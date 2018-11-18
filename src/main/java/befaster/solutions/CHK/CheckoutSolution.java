443f5113a294bef86fad41cf1ad92da34b4940b4
sbx_user1060 <sbx_user1060@ip-10-129-33-244.eu-west-2.compute.internal>
2018-11-18 23:08:44 +0000

    Sun Nov 18 23:08:44 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -33,33 +33,35 @@ public class CheckoutSolution {
 	         }
         }
         
-         
+          //5A offer
             int numberOfGroupA = counterA/5;
             int numberRemainA =  counterA%5;
             counterA=counterA-numberRemainA;
             total=total+(numberOfGroupA*200);
             
+          //3A offer
             numberOfGroupA = counterA/3;
             numberRemainA =  counterA%3;
             total=total+((numberOfGroupA*130)+(numberRemainA*50));
-            
+           
+          //E Calculation
             int numberOfGroupE = counterE/2;
             int numberRemainE=  counterE%2;
             total=total+(counterE*40);
-            
             counterB=counterB-numberOfGroupE;
+            
+          //B Calculation
             
             int numberOfGroupB = counterB/2;
             int numberRemainB =  counterB%2;
-            
-         
-            
             total=total+((numberOfGroupB*45)+(numberRemainB*30));
+            
             return total;
             
     }    
     
 }
+
 
 
 
