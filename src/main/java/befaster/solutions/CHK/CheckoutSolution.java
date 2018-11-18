3020cc467c056809caeb9c46ebfb0d934bf65cea
sbx_user1060 <sbx_user1060@ip-10-129-33-244.eu-west-2.compute.internal>
2018-11-18 21:34:47 +0000

    Sun Nov 18 21:34:47 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,19 +1,29 @@
 package befaster.solutions.CHK;
 
+import java.util.List;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    public Integer checkout(List<String> skus) {
+        
        int total = 0;
+       int counterA=0;
+       int counterB=0;
         for(String x: skus){
             if (x.equals("A")){
-                total += 50;
+               
+               total += 50;
             }else if (x.equals("B")){
                 total += 30;
             }else if (x.equals("C")){
                 total += 20;
             }
+        else if (x.equals("D")){
+            total += 15;
         }
+            
+        
     }
 }
+
