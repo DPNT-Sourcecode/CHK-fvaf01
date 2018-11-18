6d20508c29b152d8d66ce15432829414c13b4c01
sbx_user1060 <sbx_user1060@ip-10-129-33-244.eu-west-2.compute.internal>
2018-11-18 21:49:47 +0000

    Sun Nov 18 21:49:47 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,18 +12,25 @@ public class CheckoutSolution {
        int counterB=0;
         for(String x: skus){
             if (x.equals("A")){
-               
-               total += 50;
+              counterA++;
             }else if (x.equals("B")){
-                total += 30;
+                counterB++;
             }else if (x.equals("C")){
                 total += 20;
             }
-        else if (x.equals("D")){
+            else if (x.equals("D")){
             total += 15;
         }
+            int numberOfGroupA = counterA/3;
+            int numberRemainA =  counterA%3;
+            int numberOfGroupB = counterB/2;
+            int numberRemainB =  counterB%2;
+            
+            total=total+((numberOfGroupA*130)+(numberRemainA*50))+((numberOfGroupB*45)+(numberRemainB*30));
+            return total;
             
         
     }
 }
+
 
