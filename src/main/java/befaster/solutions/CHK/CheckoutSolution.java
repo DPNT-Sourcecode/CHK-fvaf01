2e52a9fbc2a54b537e84eba8f47aab621be19b4e
sbx_user1059 <sbx_user1059@ip-10-129-123-84.eu-west-2.compute.internal>
2018-11-30 18:55:43 +0000

    Fri Nov 30 18:55:43 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -177,12 +177,11 @@ public class CheckoutSolution {
 				
 				int numberOfGroupU = v / 3;
 				int numberRemainU = v % 3;
-				if(numberRemainU>1 && numberOfGroupU>0) 
-				  total = total + (numberOfGroupU*3*40)-((numberRemainU-1)*40);
-				//else
-				//total = total + (v * 40);
-				//total = total +(numberOfGroupU*3*40)+(numberRemainU*40);
-				total = total + (v * 40)-(numberOfGroupU*40);
+				if(numberRemainU>0 && numberOfGroupU>0) 
+				  total = total + (numberOfGroupU*3*40)+((numberRemainU-1)*40);
+				else
+				
+				total = total +(numberOfGroupU*3*40)+(numberRemainU*40);
 				
 
 			}
@@ -238,4 +237,5 @@ public class CheckoutSolution {
 }
     
 }
+
 
